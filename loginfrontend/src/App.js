645163ac7a61a98eabc9826d28d147be846f046b
AbhishekCode1 <shivthaku166@gmail.com>
2023-08-04 13:10:20 +0530

    mern

---
+++ loginfrontend/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css'
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+
+function App() {
+
+  const [ user] = useState({})
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            {
+              user && user._id ? <Homepage setLoginUser={<setLoginUser/>} /> : <Login setLoginUser={<setLoginUser/>}/>
+            }
+          </Route>
+          <Route  exact path="/login">
+            <Login setLoginUser={<setLoginUser/>}/>
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
